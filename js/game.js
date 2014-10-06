84abc45a330182465b5f63ed7cfc61d26b02e05e
Ranadeep Polavarapu <pola3120@mylaurier.ca>
2014-10-06 16:59:06 -0400

    Update game.js - Added Debug references.
    Included developer assisting debug logging via console.

--- js/game.js
+++ js/game.js
@@ -3,12 +3,13 @@ var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
+console.log('[DEBUG]: canvas.width=' + canvas.width + ' canvas.height=' + canvas.height);
 document.body.appendChild(canvas);
 
 // Background image
 var bgReady = false;
 var bgImage = new Image();
-bgImage.onload = function () {
+bgImage.onload = function() {
     bgReady = true;
 };
 bgImage.src = "images/background.png";
@@ -16,7 +17,7 @@ bgImage.src = "images/background.png";
 // Hero image
 var heroReady = false;
 var heroImage = new Image();
-heroImage.onload = function () {
+heroImage.onload = function() {
     heroReady = true;
 };
 heroImage.src = "images/hero.png";
@@ -24,7 +25,7 @@ heroImage.src = "images/hero.png";
 // Monster image
 var monsterReady = false;
 var monsterImage = new Image();
-monsterImage.onload = function () {
+monsterImage.onload = function() {
     monsterReady = true;
 };
 monsterImage.src = "images/monster.png";
@@ -39,16 +40,16 @@ var monstersCaught = 0;
 // Handle keyboard controls
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
+addEventListener("keydown", function(e) {
     keysDown[e.keyCode] = true;
 }, false);
 
-addEventListener("keyup", function (e) {
+addEventListener("keyup", function(e) {
     delete keysDown[e.keyCode];
 }, false);
 
 // Reset the game when the player catches a monster
-var reset = function () {
+var reset = function() {
     hero.x = canvas.width / 2;
     hero.y = canvas.height / 2;
 
@@ -58,7 +59,7 @@ var reset = function () {
 };
 
 // Update game objects
-var update = function (modifier) {
+var update = function(modifier) {
     if (38 in keysDown) { // Player holding up
         hero.y -= hero.speed * modifier;
     }
@@ -78,11 +79,12 @@ var update = function (modifier) {
     ) {
         ++monstersCaught;
         reset();
+        console.log("[DEBUG]: Goblin caught");
     }
 };
 
 // Draw everything
-var render = function () {
+var render = function() {
     if (bgReady) {
         ctx.drawImage(bgImage, 0, 0);
     }
@@ -104,7 +106,7 @@ var render = function () {
 };
 
 // The main game loop
-var main = function () {
+var main = function() {
     var now = Date.now();
     var delta = now - then;
 
@@ -119,9 +121,22 @@ var main = function () {
 
 // Cross-browser support for requestAnimationFrame
 var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame || w.oRequestAnimationFrame;
 
 // Let's play this game!
 var then = Date.now();
 reset();
 main();
+
+
+// DEBUG: Display the window's height and width just for the developer's reference.
+console.log("%c[DEBUG]: Window - height=" + window.innerHeight + ", width=" + window.innerWidth, 'font-weight: bold; color: red;');
+
+// DEBUG: Mouse to window coordinates logging in console. This will allow us to see coordinates for the entire window and the canvas.
+window.addEventListener('mousedown', function(e) {
+    var pos = {
+        x: e.pageX - 0,
+        y: e.pageY - 0
+    };
+    console.log("%c[DEBUG]: " + "x=" + pos.x + ", y=" + pos.y, 'font-weight: bold; color: blue;');
+}, false);
