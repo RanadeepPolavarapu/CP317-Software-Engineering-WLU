5d39564ce2e06c7ffe323c99d00b2a6b29cf0e75
Ranadeep Polavarapu <ranadeep@icloud.com>
2014-11-15 10:48:29 -0500

    Added android platform and build.

---
+++ g4a2/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes.dex : \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes/ca/wlu/g4a2/BuildConfig.class \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes/ca/wlu/g4a2/CordovaApp.class \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes/ca/wlu/g4a2/R$attr.class \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes/ca/wlu/g4a2/R$drawable.class \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes/ca/wlu/g4a2/R$string.class \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes/ca/wlu/g4a2/R$xml.class \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/classes/ca/wlu/g4a2/R.class \
+/Users/RanadeepPolavarapu/GitHub/CP317-Assignment-2/g4a2/platforms/android/ant-build/dexedLibs/classes-f8adf66a30b1fa0631aeb7eb604dce0e.jar \
