46e43974db6824ed11a566374166080d60ec323a
Ranadeep Polavarapu <ranadeep@icloud.com>
2014-11-17 16:49:04 -0500

    Update index.html and core.js

--- g4a3/www/js/core.js
+++ g4a3/www/js/core.js
@@ -1,16 +1,26 @@
 //  ===============================================================
-// 	Title: core.js
-// 	Project: Assignment #3
-// 	Date: Tuesday, Nov. 18, 2014
-// 	Written for: CP317-A
-// 	Authors: (Group #4)
-// 		Ranadeep Polavarapu - (120443120) - (pola3120@mylaurier.ca)
-// 		Bruno Salapic - (100574460) - (sala4460@mylaurier.ca)
-// 		Lee Glendenning - (120289190) - (glen9190@mylaurier.ca)
-// 		Ryan Burke - (120543180) - (burk3180@mylaurier.ca)
+//  Title: core.js
+//  Project: Assignment #3
+//  Date: Tuesday, Nov. 18, 2014
+//  Written for: CP317-A
+//  Authors: (Group #4)
+//      Ranadeep Polavarapu - (120443120) - (pola3120@mylaurier.ca)
+//      Bruno Salapic - (100574460) - (sala4460@mylaurier.ca)
+//      Lee Glendenning - (120289190) - (glen9190@mylaurier.ca)
+//      Ryan Burke - (120543180) - (burk3180@mylaurier.ca)
 //  ===============================================================
 
-$(function() { // Wait till DOM is ready.
+/**
+ * Makes a HTTP GET request to the postal code database server and determines the location
+ * of the postal code given a valid postal code. Then, the response is displayed in an alert.
+ * If postal code is invalid, then the corresponding error response from server is also
+ * displayed in an alert.
+ *
+ * Better to declare a function like this as this way the function is declared and initialized
+ * during parsetime rather than runtime for a "var function()" declaration. This is better for
+ * on DOM ready.
+ */
+function postalCodeLookup() {
     $('#postal-code-lookup-form').submit(function(event) {
 
         // Lock and prevent the default form from actually submitting.
@@ -30,4 +40,8 @@ $(function() { // Wait till DOM is ready.
             },
         });
     });
+}
+
+$(function() { // Wait till DOM (Document Object Model) is ready.
+    postalCodeLookup();
 });
